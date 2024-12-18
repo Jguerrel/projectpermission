e719c6845575b31f226270572749880fa99ac0a0
Jguerrel <103289719+Jguerrel@users.noreply.github.com>
2024-12-18 09:24:53 -0500

    Update office

--- app/Http/Requests/UpdateMicrosoftofficeRequest.php
+++ app/Http/Requests/UpdateMicrosoftofficeRequest.php
@@ -3,7 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-
+use Illuminate\Validation\Rule;
 class UpdateMicrosoftofficeRequest extends FormRequest
 {
     /**
@@ -11,7 +11,7 @@ class UpdateMicrosoftofficeRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,24 @@ class UpdateMicrosoftofficeRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required',Rule::unique('microsoftoffice','name')->ignore($this->microsoftoffice)]
+
+        ];
+    }
+    public function messages()
+    {
+        return [
+
+            'name.required' => 'El :attribute es obligatorio.',
+            'name.unique' => 'El :attribute ya existe.',
+        ];
+
+    }
+
+    public function attributes()
+    {
+        return [
+            'name' => 'Office ',
         ];
     }
 }
--- resources/views/microsoftoffices/edit.blade.php
+++ resources/views/microsoftoffices/edit.blade.php
@@ -1,1 +1,64 @@
 @extends('adminlte::page')
+
+
+@section('content')
+
+<section class="content-header" >
+            <div class="container-fluid">
+                <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Editar Office</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Microsoft Office</a></li>
+                    <li class="breadcrumb-item active">Editar Office</li>
+                    </ol>
+                </div>
+                </div>
+            </div><!-- /.container-fluid -->
+</section>
+<div class="row justify-content-center">
+    <div class="col-md-12">
+        <div class="card car-info">
+                <div class="card-header">
+                    <div class="float-start">
+                        Editar Office
+                    </div>
+                    <div class="float-end">
+                        <a href="{{ route('microsoftoffices.index') }}" class="btn btn-info btn-sm">&larr; Volver</a>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('microsoftoffices.update', $microsoftoffice->id) }}" method="post">
+                    @csrf
+                    @method("PUT")
+
+                        <div class="mb-3 row">
+                            <label for="name" class="col-md-4 col-form-label text-md-end text-start">Nombre</label>
+                            <div class="col-md-6">
+                            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ $microsoftoffice->name }}">
+                                @if ($errors->has('name'))
+                                    <span class="text-danger">{{ $errors->first('name') }}</span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <label for="status" class="col-md-4 col-form-label text-md-end text-start">Estado</label>
+                             <div class="col-md-6">
+                             <input type="hidden" name="status" value="0">
+                             <input type="checkbox" class="@error('status') is-invalid @enderror" name="status" id='status' value="1" {{ $microsoftoffice->status ? 'checked' : '' }} />
+                                @if ($errors->has('status'))
+                                        <span class="text-danger">{{ $errors->first('status') }}</span>
+                                    @endif
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                           <input type="submit" class="col-md-3 offset-md-5 btn btn-info" value="Actualizar Cargo">
+                        </div>
+                </form>
+                </div>
+        </div>
+    </div>
+</div>
+@endsection
